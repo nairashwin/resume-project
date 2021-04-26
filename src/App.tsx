1b70d26e4eeb4be7bf5b3d07b6423606717c51c4
ashwin <nair.ashwin21@gmail.com>
2021-04-26 12:39:24 +0530

    Created a sample cv with experience

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState<any[]>([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const respone = await fetch(url);
+    const newJobs = await respone.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading..</h1>
+      </section>
+    );
+  }
+  console.log(jobs);
+
+  const { company, dates, duties, title } = jobs[value];
+
+  console.log(duties)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        {/* btn-container  */}
+        <div className="btn-container">
+          {
+            jobs.map((item, index) => {
+              return (
+                <button className={`job-btn ${index === value && "active-btn"}`} key = { item.id } onClick = {() => setValue(index)}>
+                {/* <button className={`job-btn ${(index === value)?"active-btn":null}`} key={item.id} onClick={() => setValue(index)}> */}
+                  {item.company}
+                </button>
+              );
+            })
+          }
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{dates}</p>
+          {duties.map((duty: string, index: number) => {
+            return (
+              <div key={index} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section >
+
   );
 }
 
-export default App;
+export default App
